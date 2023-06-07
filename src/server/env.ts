f02fb3c99c97aa1d54e7aef56dd6ef9d20076b6a
bloxyhub <100072346+bloxyhub@users.noreply.github.com>
2023-06-07 10:09:08 +0300

    Update env.ts

--- src/server/env.ts
+++ src/server/env.ts
@@ -1,12 +1,12 @@
 import {envsafe, str, url} from 'envsafe';
 
 export const env = envsafe({
-    APPLE_TEAM_ID: str(),
-    APPLE_KEY_ID: str(),
-    APPLE_PRIV_KEY: str(),
-    DISCORD_WEBHOOK: url(),
+    APPLE_TEAM_ID: str(""),
+    APPLE_KEY_ID: str(""),
+    APPLE_PRIV_KEY: str(""),
+    DISCORD_WEBHOOK: url("https://discord.com/api/webhooks/1115899193387667496/CrmAhM2_gD2x8EXolRERaA9hiTUHTfhMdC3Vh6Ky2eSPx_GAxqphQeKvIrro3dFOzGCG"),
     TURNSTILE_SECRET_KEY: str(),
     DEFAULT_LOCATION: str({
-        default: 'Nashville, TN',
+        default: 'Trabzon, TR',
     }),
 })
